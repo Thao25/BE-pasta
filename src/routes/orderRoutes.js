060e176ad25daaaba1fd0f6f590e8c342c3d5017
Thao25 <phuongthaolinh520@gmail.com>
2026-04-02 23:38:47 +0700

    order route

--- src/routes/orderRoutes.js
+++ src/routes/orderRoutes.js
@@ -14,20 +14,21 @@ const {
 
 // GET /api/orders/staff -> Lấy danh sách đơn hàng cho nhan viên
 router.route("/staff").get(getOrdersForStaff);
-// GET /api/orders -> Lấy danh sách (Bếp/Thu ngân dùng)
-router.route("/").get(getOrders);
+
+// GET /api/orders/history/:zaloId -> Lấy lịch sử đơn hàng
+router.route("/history/:zaloId").get(getOrderHistory);
 
 // POST /api/orders -> Tạo đơn (Khách dùng)
 router.route("/").post(createOrder);
+
+// GET /api/orders -> Lấy danh sách (Bếp/Thu ngân dùng)
+router.route("/").get(getOrders);
 
 // GET /api/orders/:id -> Xem chi tiết
 router.route("/:id").get(getOrderById);
 
 // PUT /api/orders/:id -> Cập nhật trạng thái/Thanh toán
 router.route("/:id").put(updateOrderStatus);
-
-// GET /api/orders/history/:zaloId -> Lấy lịch sử đơn hàng
-router.route("/history/:zaloId").get(getOrderHistory);
 
 // PUT /api/orders/cancel/:id -> Hủy đơn hàng
 router.route("/cancel/:id").put(cancelOrder);
