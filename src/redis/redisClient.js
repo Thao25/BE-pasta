4172944837f27a93d81e5fe61a86834679501f33
Thao25 <phuongthaolinh520@gmail.com>
2026-05-14 23:52:40 +0700

    redist cache cloud

--- src/redis/redisClient.js
+++ src/redis/redisClient.js
@@ -1,7 +1,9 @@
+// import { Redis } from '@upstash/redis'
 const redis = require("redis");
 
 const redisClient = redis.createClient({
-  url: "redis://localhost:6379",
+  url: process.env.REDIS_URL,
+  token: process.env.REDIS_TOKEN,
 });
 
 redisClient.on("error", (err) => {
