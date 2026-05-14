a18c4b2be5d9d3f55ea67994cab51189083172f9
Thao25 <phuongthaolinh520@gmail.com>
2026-05-15 00:00:24 +0700

    redist cache cloud2

--- src/redis/redisClient.js
+++ src/redis/redisClient.js
@@ -1,18 +1,20 @@
-// import { Redis } from '@upstash/redis'
 const redis = require("redis");
 
 const redisClient = redis.createClient({
   url: process.env.REDIS_URL,
-  token: process.env.REDIS_TOKEN,
 });
 
 redisClient.on("error", (err) => {
-  console.log("Redis Error:", err);
+  console.log("Redis Error:", err.message);
 });
 
 (async () => {
-  await redisClient.connect();
-  console.log("Redis Connected");
+  try {
+    await redisClient.connect();
+    console.log("Redis Connected");
+  } catch (err) {
+    console.log("Redis Connect Failed");
+  }
 })();
 
 module.exports = redisClient;
